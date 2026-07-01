3f73f7df420d0e77ec1a4929c2c91cdf044e86ed
NeoAtlantis <contact@chaobai.li>
2026-07-01 23:42:49 +0200

    written command UpdateExtra

--- na_atlib/ATSHA204/_CMD_UPDATEEXTRA.py
+++ na_atlib/ATSHA204/_CMD_UPDATEEXTRA.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+from enum import IntEnum
 from ..command import CommandPacket
 
 class CMD_UPDATEEXTRA(CommandPacket):
-    def __init__(self):
+
+    class Mode(IntEnum):
+        UPDATE_BYTE_84                  = 0
+        UPDATE_BYTE_85                  = 1
+        DECREMENT_LIMITED_USE_COUNTER   = 0b10
+
+    def __init__(self, mode: Mode, newvalue):
+        assert type(mode) is CMD_UPDATEEXTRA.Mode
+        assert type(newvalue) is int
+
         CommandPacket.__init__(self,
             0x20,                       # OpCode
-            0x00, (0x00, 0x00),
+            int(mode), (newvalue & 0xFF, 0x00),
             response_size = 1
         )
